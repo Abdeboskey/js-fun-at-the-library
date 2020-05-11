a81af75edf59d765a196bc3f4f44bf8283b31bb4
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-05-10 18:23:59 -0600

    Complete library and library test

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,41 @@
 
+function createLibrary(libraryName) {
+  return { name: libraryName,
+           shelves: { fantasy: [],
+                      fiction: [],
+                      nonFiction: []
+                    }
+         };
+}
+
+function addBook(library, book) {
+  if (book.genre === "fantasy") { library.shelves.fantasy.push(book); }
+  if (book.genre === "fiction") { library.shelves.fiction.push(book); }
+  if (book.genre === "nonFiction") { library.shelves.nonFiction.push(book); }
+}
+
+function checkoutBook(library, book) {
+  var notification = "";
+  function lookForBook(shelf) {
+    for (var i = 0; i < shelf.length; i++) {
+      if (shelf[i].title.includes(book)) {
+        shelf.splice(i, 1);
+        notification += "got it";
+      }
+    }
+  }
+  lookForBook(library.shelves.fantasy);
+  lookForBook(library.shelves.fiction);
+  lookForBook(library.shelves.nonFiction);
+  if (notification) {
+    return `You have now checked out ${book} from the ${library.name}`;
+  } else {
+    return `Sorry, there are currently no copies of ${book} available at the ${library.name}`;
+  }
+}
 
 module.exports = {
-  // createLibrary: createLibrary,
-  // addBook: addBook,
-  // checkoutBook: checkoutBook
+  createLibrary: createLibrary,
+  addBook: addBook,
+  checkoutBook: checkoutBook
 };
